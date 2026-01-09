708c403894040848b2d8d46cbb9cdfc91dece275
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-09 13:24:26 -0300

    feat: implement usePomodoro hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { usePomodoro } from "./hooks/usePomodoro";
+import { formatTime, minToMs } from "./utils/time";
+
 export default function App() {
-  return <h1 className="title">Hello, World!</h1>;
+  const {
+    state: { mode, timeLeft, completedCycles },
+    actions: { start, pause, reset }
+    // } = usePomodoro(minToMs(25), minToMs(5), minToMs(15), 4);
+  } = usePomodoro(2000, 2000, 2000, 4); // just for testing purposes
+  return (
+    <>
+      <h1>Mode: {mode}</h1>
+      <h2>Completed cycles: {completedCycles}</h2>
+      <div>{formatTime(timeLeft)}</div>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
 }
