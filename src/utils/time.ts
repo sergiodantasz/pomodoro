7701c7dee101ecb339ebb71a5ebfb9cac05abe51
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-09 13:22:11 -0300

    feat(utils): create functions to improve time management

---
+++ src/utils/time.ts
@@ -0,0 +1,10 @@
+export function formatTime(timeMs: number): string {
+  const totalSeconds = Math.floor(timeMs / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+export function minToMs(minutes: number): number {
+  return minutes * 60 * 1000;
+}
